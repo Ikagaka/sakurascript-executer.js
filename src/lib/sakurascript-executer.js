8f9c6a1e51c5df1d29149643f192df0a2bebd550
Narazaka <info@narazaka.net>
2016-12-04 19:59:34 +0900

    to ts

--- src/lib/sakurascript-executer.js
+++ src/lib/sakurascript-executer.js
@@ -1,157 +1,192 @@
-import {EventEmitter} from 'events';
-import {SakuraScript, SakuraScriptToken} from 'sakurascript';
+import {EventEmitter} from "events";
+import {SakuraScript, SakuraScriptToken} from "sakurascript";
 
 /**
  * SakuraScript Executer
  */
 export class SakuraScriptExecuter extends EventEmitter {
+  private _quick: boolean;
+  private _talkWait: number;
+  private _executing: boolean;
+  private _executeId: number;
+  private _willAbortId: number;
+  private _waitUntilActionId: "_balloonClicked" | number | null;
+  private _waitPeriod: number | null;
+  private _executeStartTime: Date;
+  private _quickSection: boolean;
+  private _currentWait: (() => void) | null;
+  private _balloonClicked: (() => void) | null;
+  private _animationFinished: {[id: string]: (() => void) | null};
+
   /**
    * constructor
-   * @param {object} options options
+   * @param options options
+   * @param quick quick section flag
+   * @param talkWait talk wait
    */
-  constructor(options = {}) {
+  constructor(options: {quick?: boolean, talkWait?: number} = {}) {
     super();
     this._quick = options.quick || false;
-    this._talk_wait = options.talk_wait || 0;
+    this._talkWait = options.talkWait || 0;
     this._executing = false;
-    this._execute_id = 0; // TODO: 永遠に動かし続けることはできない もっといい手がないか
-    this._will_abort_id = 0;
+    this._executeId = 0; // TODO: 永遠に動かし続けることはできない もっといい手がないか
+    this._willAbortId = 0;
+    this._animationFinished = {};
   }
 
   /**
    * quick mode
-   * @type {Boolean}
    */
   get quick() { return this._quick; }
 
   /**
    * quick mode
-   * @type {Boolean}
    */
   set quick(value) { this._quick = value; }
 
   /**
    * default talk wait
-   * @type {number}
    */
-  get talk_wait() { return this._talk_wait; }
+  get talkWait() { return this._talkWait; }
+
+  /**
+   * default talk wait
+   */
+  set talkWait(value) { this._talkWait = value; }
 
   /**
    * true if executing
-   * @type {Boolean}
    */
   get executing() { return this._executing; }
 
+  on(event: "beginExecute", listener: () => void): this;
+  on(event: "execute", listener: (token: SakuraScriptToken) => void): this;
+  on(event: "endExecute", listener: (aborted: boolean) => void): this;
+  on(event: string, listener: Function) {
+    return super.on(event, listener);
+  }
+
+  emit(event: "beginExecute"): boolean;
+  emit(event: "execute", token: SakuraScriptToken): boolean;
+  emit(event: "endExecute", aborted: boolean): boolean;
+  emit(event: string, ...args: any[]) {
+    return super.emit(event, args);
+  }
+
   /**
    * execute sakura script
-   * @param {string} script sakura script
-   * @emits {begin_execute()} sakurascript execute begin event
+   * @param script sakura script
+   * @emits {beginExecute()} sakurascript execute begin event
    * @emits {execute(token)} sakurascript execute token event
-   * @emits {end_execute(is_abort)} sakurascript execute end event
-   * @return {void}
+   * @emits {endExecute(aborted)} sakurascript execute end event
    */
-  async execute(script) {
-    this.abort_execute(); // abort previous session
-    const execute_id = ++this._execute_id;
-    const sakurascript = SakuraScript.parse(script);
-    this.emit('begin_execute');
-    this._initialize_execute_state();
-    for (const token of sakurascript.tokens) {
-      if (this._wait_until_action_name) {
-        await this._wait_until_action(this._wait_until_action_name);
-        this._wait_until_action_name = null;
+  async execute(script: string) {
+    this.abortExecute(); // abort previous session
+    const executeId = ++this._executeId;
+    const sakuraScript = SakuraScript.parse(script);
+    this.emit("beginExecute");
+    this._initializeExecuteState();
+    for (const token of sakuraScript.tokens) {
+      if (this._waitUntilActionId) {
+        await this._waitUntilAction(this._waitUntilActionId);
+        this._waitUntilActionId = null;
       } else if (!this.quick) {
-        if (this._wait_period != null) {
-          await this._wait(this._wait_period);
-          this._wait_period = null;
-        } else if (token instanceof SakuraScriptToken.Char && !this._quick_section) {
-          await this._wait(this.talk_wait);
+        if (this._waitPeriod != null) {
+          await this._wait(this._waitPeriod);
+          this._waitPeriod = null;
+        } else if (token instanceof SakuraScriptToken.Char && !this._quickSection) {
+          await this._wait(this.talkWait);
         }
       }
-      if (this._will_abort_id >= execute_id) break;
-      this.emit('execute', token);
+      if (this._willAbortId >= executeId) break;
+      this.emit("execute", token);
       if (token instanceof SakuraScriptToken.Char) {
         continue;
       } else if (token instanceof SakuraScriptToken.PlayAnimationWait) {
-        this._wait_until_action_name = `_animation_finished_${token.animation}`;
+        this._waitUntilActionId = token.animation;
       } else if (token instanceof SakuraScriptToken.WaitAnimationEnd) {
-        this._wait_until_action_name = `_animation_finished_${token.id}`;
+        this._waitUntilActionId = token.id;
       } else if (token instanceof SakuraScriptToken.WaitFromBeginning) {
-        const period = new Date() - this._execute_start_time;
-        if (period > 0) this._wait_period = period;
+        const period = new Date().getTime() - this._executeStartTime.getTime();
+        if (period > 0) this._waitPeriod = period;
       } else if (token instanceof SakuraScriptToken.ResetBeginning) {
-        this._execute_start_time = new Date();
+        this._executeStartTime = new Date();
       } else if (token instanceof SakuraScriptToken.WaitClick) {
-        this._execute_start_time = new Date();
-        this._wait_until_action_name = '_balloon_clicked';
+        this._executeStartTime = new Date();
+        this._waitUntilActionId = "_balloonClicked";
       } else if (token instanceof SakuraScriptToken.SimpleWait) {
-        this._wait_period = token.period * 50;
+        this._waitPeriod = token.period * 50;
       } else if (token instanceof SakuraScriptToken.PreciseWait) {
-        this._wait_period = token.period;
+        this._waitPeriod = token.period;
       } else if (token instanceof SakuraScriptToken.ToggleQuick) {
-        this._quick_section = !this._quick_section;
+        this._quickSection = !this._quickSection;
       }
     }
-    this._finalize_execute_state();
-    this.emit('end_execute', this._will_abort_id >= execute_id);
-  }
-
-  _initialize_execute_state() {
-    this._executing = true;
-    this._wait_period = 0;
-    this._wait_until_action_name = null;
-    this._quick_section = false;
-    this._current_wait = null;
-    this._execute_start_time = new Date();
-  }
-
-  _finalize_execute_state() {
-    this._executing = false;
-  }
-
-  async _wait(period) {
-    return new Promise((resolve) => {
-      this._current_wait = resolve;
-      setTimeout(() => resolve(period), period);
-    }).then(() => {
-      this._current_wait = null;
-    });
-  }
-
-  async _wait_until_action(name) {
-    return new Promise((resolve) => {
-      this._current_wait = resolve;
-      this[name] = resolve;
-    }).then(() => {
-      this._current_wait = null;
-      delete this[name];
-    });
+    this._finalizeExecuteState();
+    this.emit("endExecute", this._willAbortId >= executeId);
   }
 
   /**
    * call when balloon clicked
-   * @return {void}
    */
-  balloon_clicked() {
-    if (this._balloon_clicked) this._balloon_clicked();
+  balloonClicked() {
+    if (this._balloonClicked) this._balloonClicked();
   }
 
   /**
    * call when animation finished
-   * @param {number} animation_id animation id
-   * @return {void}
+   * @param animationId animation id
    */
-  animation_finished(animation_id) {
-    const done = this[`_animation_finished_${animation_id}`];
+  animationFinished(animationId: number) {
+    const done = this._animationFinished[animationId];
     if (done) done();
   }
 
   /**
    * call when you want to abort
-   * @return {void}
    */
-  abort_execute() {
-    this._will_abort_id = this._execute_id;
-    if (this._current_wait) this._current_wait();
+  abortExecute() {
+    this._willAbortId = this._executeId;
+    if (this._currentWait) this._currentWait();
+  }
+
+  private _initializeExecuteState() {
+    this._executing = true;
+    this._waitPeriod = 0;
+    this._waitUntilActionId = null;
+    this._quickSection = false;
+    this._currentWait = null;
+    this._executeStartTime = new Date();
+  }
+
+  private _finalizeExecuteState() {
+    this._executing = false;
+  }
+
+  private async _wait(period: number) {
+    await new Promise((resolve) => {
+      this._currentWait = resolve;
+      setTimeout(() => resolve(period), period);
+    }).then(() => {
+      this._currentWait = null;
+    });
+  }
+
+  private async _waitUntilAction(_waitUntilActionId: "_balloonClicked" | number) {
+    await new Promise((resolve) => {
+      this._currentWait = resolve;
+      if (_waitUntilActionId === "_balloonClicked") {
+        this._balloonClicked = resolve;
+      } else {
+        this._animationFinished[_waitUntilActionId] = resolve;
+      }
+    }).then(() => {
+      this._currentWait = null;
+      if (_waitUntilActionId === "_balloonClicked") {
+        this._balloonClicked = null;
+      } else {
+        this._animationFinished[_waitUntilActionId] = null;
+      }
+    });
   }
 }
